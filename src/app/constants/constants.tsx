53f3e3d27513bb9093bd74d68592cdbd9de544ac
vedangp1998 <vedang.prajapati18@gmail.com>
2025-04-05 05:58:49 +0530

    bug-fix

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -2,6 +2,6 @@ export const NavLinks = [
   { key: "home", label: "Home", href: "#home" },
   { key: "about", label: "About", href: "#about" },
   { key: "services", label: "Infrastructure", href: "#infrastructure" },
-  { key: "services", label: "Team", href: "#team" },
+  { key: "team", label: "Team", href: "#team" },
   { key: "contact", label: "Contact", href: "#contact" },
 ];
