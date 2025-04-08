5107c0e00f1c13fd0d0dcb8f716d8a39467a1990
vedangp1998 <vedang.prajapati18@gmail.com>
2025-04-08 16:49:59 +0530

    fix-routing

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,30 +1,23 @@
 export const NavLinks = {
   default: [
-    { label: "Home", href: "#home" },
+    { label: "Home", href: "/" },
     { label: "About", href: "#about" },
     { label: "Infrastructure", href: "#infrastructure" },
     { label: "Team", href: "#team" },
     { label: "Contact", href: "#contact" },
   ],
   Pg: [
-    { label: "Home", href: "#home" },
+    { label: "Home", href: "/" },
     { label: "About", href: "#about" },
-    { label: "Infrastructure", href: "#infrastructure" },
-    { label: "Team", href: "#team" },
+    // { label: "Infrastructure", href: "#infrastructure" },
+    // { label: "Team", href: "#team" },
     { label: "Contact", href: "#contact" },
   ],
   utex: [
-    { label: "Home", href: "#home" },
+    { label: "Home", href: "/" },
     { label: "About", href: "#about" },
-    { label: "Infrastructure", href: "#infrastructure" },
-    { label: "Team", href: "#infrastructure" },
-    { label: "Contact", href: "#contact" },
-  ],
-  exports: [
-    { label: "Home", href: "#home" },
-    { label: "About", href: "#about" },
-    { label: "Infrastructure", href: "#infrastructure" },
-    { label: "Team", href: "#infrastructure" },
+    // { label: "Infrastructure", href: "#infrastructure" },
+    // { label: "Team", href: "#infrastructure" },
     { label: "Contact", href: "#contact" },
   ],
 };
